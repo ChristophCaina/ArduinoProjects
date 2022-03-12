e5c60a4e9f8197fc726e0c177595da160bcae8ec
Christoph <Christoph@Caina.de>
2022-03-12 08:44:21 +0100

    Update ecto_1.ino

--- arduino/ecto_1/ecto_1.ino
+++ arduino/ecto_1/ecto_1.ino
@@ -1,5 +1,26 @@
+/*
+ * 
+ *   Controller for the Eaglemoss Ecto-1 Model Kit
+ *   by Christoph Caina
+ *   
+ */
+
+#include <PCF8591.h>                  // Analog In-/Output extension
+#include <PCF8574.h>                  // Digital In-/Output extension
+#include <IRremote.hpp>               // IR remote
+
+bool debug = true;                   // for DebugOption we want to shorten a specific PIN. default value is true
+const byte debugPin = D2;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(debugPin, INPUT);
+
+  if(!digitalRead(debugPin)) {
+    debug = false;
+  }
+  else {
+    Serial.begin(115200);
+  }
 }
 
 void loop() {
