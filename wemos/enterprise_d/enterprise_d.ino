2958bc742a45fdcd706dcc55d21a9bc97a43e180
Christoph <Christoph@Caina.de>
2022-03-14 16:47:50 +0100

    Update enterprise_d.ino

--- wemos/enterprise_d/enterprise_d.ino
+++ wemos/enterprise_d/enterprise_d.ino
@@ -13,7 +13,9 @@
 const byte debugPin = D2;
 bool debug;                           // for DebugOption we want to shorten a specific PIN. default value is true
 unsigned long cpTimeout = 120;        // seconds until the CP will time-out
-char* wifiAutoSSID = "NCC1701D"       // our 'default' SSID when the CP starts
+char* wifiAutoSSID = "NCC1701D";      // our 'default' SSID when the CP starts
+
+WiFiManager wifiManager;
 
 void setup() {
   pinMode(debugPin, INPUT);
@@ -30,35 +32,27 @@ void setup() {
   WiFi.mode(WIFI_STA);                // explicitly set mode, esp defaults to STA+AP 
   
   if(debug) {
-    wm.resetSettings();               // for debug and testing, we will reset the WiFiManager if debug is set to true
+    wifiManager.resetSettings();               // for debug and testing, we will reset the WiFiManager if debug is set to true
   }
   
-  wm.setConfigPortalBlocking(false);
-  wm.setConfigPortalTimeout(cpTimeout);
+  wifiManager.setConfigPortalBlocking(false);
+  wifiManager.setConfigPortalTimeout(cpTimeout);
 
-  if(wm.autoConnect()) {
+  if(wifiManager.autoConnect(wifiAutoSSID)) {
     if(debug) {
       Serial.print("successfully connected to: ");
-      Serial.println("SSID: " + (String)wm.getWiFiSSID()));
+      Serial.println("SSID: " + (String)wifiManager.getWiFiSSID()));
     }
   }
   else {
-    if(wm.startConfigPortal(wifiAutoSSID)) {
-      if(debug) {
-        Serial.print("configuration portal started with");
-        Serial.println("SSID: " + (String)wm.getWiFiSSID()));
-      }
+    if(debug) {
+      Serial.print("failed to launch configuration portal");
+      Serial.println("restarting device!");
     }
-    else {
-      if(debug) {
-        Serial.print("failed to launch configuration portal");
-        Serial.println("restarting device!");
-      }
-      ESP.restart();
-    }
+    ESP.restart();
   }
 }
 
 void loop() {
-    wm.process();
+    wifiManager.process();
 }
